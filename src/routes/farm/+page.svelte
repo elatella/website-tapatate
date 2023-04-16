57d9ba5cf89484dc07b9ecd32b45092792676c9c
Raphaela Seeger <elatella@users.noreply.github.com>
2023-04-16 18:41:12 +0200

    Add subpages

---
+++ src/routes/farm/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import Title from '$lib/Title.svelte';
+	import backgroundImage from '$lib/images/bg-farm.jpg';
+	import { _ } from 'svelte-i18n';
+</script>
+
+<Title title={$_('farm.title')} {backgroundImage} />
