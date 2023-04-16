cbde1ea8a96db1db8f126b58f87f8726f3c3b375
Lena Fuhrimann <6780471+cloudlena@users.noreply.github.com>
2023-04-16 15:07:51 +0200

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import '../theme.postcss';
+	import '@skeletonlabs/skeleton/styles/all.css';
+	import '../app.postcss';
+	import { AppShell } from '@skeletonlabs/skeleton';
+
+	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
+	import { storePopup } from '@skeletonlabs/skeleton';
+	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+
+	import { addMessages, init, getLocaleFromQueryString } from 'svelte-i18n';
+	import de from '$lib/locales/de.json';
+	import fr from '$lib/locales/fr.json';
+	import Header from './Header.svelte';
+	addMessages('de-CH', de);
+	addMessages('fr-CH', fr);
+
+	init({
+		fallbackLocale: 'de-CH',
+		initialLocale: getLocaleFromQueryString('lang')
+	});
+</script>
+
+<AppShell>
+	<svelte:fragment slot="header">
+		<Header />
+	</svelte:fragment>
+	<slot />
+</AppShell>
---
+++ src/routes/Header.svelte
@@ -0,0 +1,63 @@
+<script lang="ts">
+	import Fa from 'svelte-fa';
+	import { AppBar, popup } from '@skeletonlabs/skeleton';
+	import { _ } from 'svelte-i18n';
+	import { faBars } from '@fortawesome/free-solid-svg-icons';
+	import logo from '$lib/images/logo_txt.png';
+
+	interface NavigationItem {
+		title: string;
+		href: string;
+	}
+
+	const navigationItems: NavigationItem[] = [
+		{ title: $_('project.title'), href: '/project' },
+		{ title: $_('farm.title'), href: '/farm' },
+		{ title: $_('participate.title'), href: '/participate' },
+		{ title: $_('gallery.title'), href: '/gallery' },
+		{ title: $_('knowledge.title'), href: '/knowledge' },
+		{ title: $_('contact.title'), href: '/contact' }
+	];
+</script>
+
+<AppBar shadow="shadow-md">
+	<svelte:fragment slot="lead">
+		<a href="/" class="flex items-center">
+			<img src={logo} alt={$_('navigation.altMountain')} height="h-12" class="h-12 mr-2" />
+		</a>
+	</svelte:fragment>
+
+	<svelte:fragment slot="trail">
+		{#each navigationItems as navigationItem (navigationItem.title)}
+			<a class="btn hidden lg:block" href={navigationItem.href}>
+				{navigationItem.title}
+			</a>
+		{/each}
+
+		<div class="lg:hidden">
+			<button
+				class="btn"
+				use:popup={{
+					event: 'focus-click',
+					target: 'small-navigation',
+					closeQuery: '.small-navigation-item'
+				}}
+			>
+				<Fa icon={faBars} />
+			</button>
+			<div class="card p-4 w-60 shadow-xl" data-popup="small-navigation">
+				<nav class="list-nav">
+					<ul>
+						{#each navigationItems as navigationItem (navigationItem.title)}
+							<li class="small-navigation-item">
+								<a href={navigationItem.href}>
+									{navigationItem.title}
+								</a>
+							</li>
+						{/each}
+					</ul>
+				</nav>
+			</div>
+		</div>
+	</svelte:fragment>
+</AppBar>
