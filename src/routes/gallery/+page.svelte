40760b5b16ef1f9102752e3f07136b50b288d9c7
Raphaela Seeger <elatella@users.noreply.github.com>
2023-04-17 23:15:16 +0200

    Add french news

--- src/routes/gallery/+page.svelte
+++ src/routes/gallery/+page.svelte
@@ -5,3 +5,19 @@
 </script>
 
 <Title title={$_('gallery.title')} {backgroundImage} />
+
+<div>
+	<h3>{$_('gallery.2022')}</h3>
+</div>
+<div>
+	<h3>{$_('gallery.2021')}</h3>
+</div>
+<div>
+	<h3>{$_('gallery.2020')}</h3>
+</div>
+<div>
+	<h3>{$_('gallery.2019')}</h3>
+</div>
+<div>
+	<h3>{$_('gallery.2018')}</h3>
+</div>
