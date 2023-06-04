750831ce4321f7fe15e5e9cce03894be203893d8
Raphaela Seeger <elatella@users.noreply.github.com>
2023-06-05 01:06:59 +0200

    Add contact(1)

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -2,6 +2,105 @@
 	import { _ } from 'svelte-i18n';
 	import Title from '$lib/Title.svelte';
 	import backgroundImage from '$lib/images/bg-contact.jpg';
+	import { writable } from 'svelte/store';
 </script>
 
 <Title title={$_('contact.title')} {backgroundImage} />
+
+<div class="w-full px-8">
+	<section class="container mx-auto flex flex-col max-w-5xl space-y-16">
+		<div class="flex flex-col md:flex-row justify-around">
+			<div>
+				<h3>{$_('contact.bank.title')}</h3>
+				<p>{$_('contact.bank.text1')}</p>
+				<p>{$_('contact.bank.text2')}</p>
+				<p>{$_('contact.bank.text3')}</p>
+				<p>{$_('contact.bank.text4')}</p>
+				<p>{$_('contact.bank.text5')}</p>
+				<p>{$_('contact.bank.text6')}</p>
+				<p>{$_('contact.bank.text7')}</p>
+			</div>
+
+			<div>
+				<h3>{$_('contact.adress.title')}</h3>
+				<p>{$_('contact.adress.text1')}</p>
+				<p>{$_('contact.adress.text2')}</p>
+				<p>{$_('contact.adress.text3')}</p>
+			</div>
+		</div>
+		<div>
+			<h3>{$_('contact.arrival.title')}</h3>
+			<br />
+		<h5> <strong>{$_('contact.arrival.bern.title')}</strong></h5>
+		<ul class="list-disc p-4 space-y-1 list-inside">
+			<li>
+			<strong>{$_('contact.arrival.bern.bold1')}</strong>
+			{$_('contact.arrival.bern.text1')}
+			<a href="https://{$_('contact.arrival.bern.link1')}">{$_('contact.arrival.bern.linkText1')}</a>
+			{$_('contact.arrival.bern.text2')}
+			<a href="https://{$_('contact.arrival.bern.link2')}">{$_('contact.arrival.bern.linkText2')}</a>
+			{$_('contact.arrival.bern.text3')}
+			</li>
+			<li>
+			<strong>{$_('contact.arrival.bern.bold2')}</strong>
+			{$_('contact.arrival.bern.text1')}
+			<a href="https://{$_('contact.arrival.bern.link1')}">{$_('contact.arrival.bern.linkText1')}</a>
+			{$_('contact.arrival.bern.text4')}
+			<a href="https://{$_('contact.arrival.bern.link2')}">{$_('contact.arrival.bern.linkText2')}</a>
+{$_('contact.arrival.bern.bracet')}
+</li>
+			<li>
+			<strong>{$_('contact.arrival.bern.bold3')}</strong>
+			{$_('contact.arrival.bern.text5')}
+			<a href="https://{$_('contact.arrival.bern.link3')}">{$_('contact.arrival.bern.linkText3')}</a>
+			{$_('contact.arrival.bern.text6')}
+</li>
+			<li>
+			<strong>{$_('contact.arrival.bern.bold4')}</strong>
+			{$_('contact.arrival.bern.text7')}
+			<a href="https://{$_('contact.arrival.bern.link4')}">{$_('contact.arrival.bern.linkText4')}</a>
+			{$_('contact.arrival.bern.text8')}
+			<a href="https://{$_('contact.arrival.bern.link5')}">{$_('contact.arrival.bern.linkText5')}</a>
+			{$_('contact.arrival.bern.dot')}
+</li>
+	</ul>
+		<h5> <strong>{$_('contact.arrival.fribourg.title')}</strong></h5>
+		<ul class="list-disc p-4 space-y-1 list-inside">
+			<li>
+			<strong>{$_('contact.arrival.fribourg.bold1')}</strong>
+			{$_('contact.arrival.fribourg.text1')}
+			<a href="https://{$_('contact.arrival.fribourg.link1')}">{$_('contact.arrival.fribourg.linkText1')}</a>
+			{$_('contact.arrival.fribourg.text2')}
+			<a href="https://{$_('contact.arrival.fribourg.link2')}">{$_('contact.arrival.fribourg.linkText2')}</a>
+			{$_('contact.arrival.fribourg.text3')}
+			</li>
+			<li>
+			<strong>{$_('contact.arrival.fribourg.bold2')}</strong>
+			{$_('contact.arrival.fribourg.text1')}
+			<a href="https://{$_('contact.arrival.fribourg.link1')}">{$_('contact.arrival.fribourg.linkText1')}</a>
+			{$_('contact.arrival.fribourg.text4')}
+			<a href="https://{$_('contact.arrival.fribourg.link2')}">{$_('contact.arrival.fribourg.linkText2')}</a>
+{$_('contact.arrival.fribourg.bracet')}
+</li>
+			<li>
+			<strong>{$_('contact.arrival.fribourg.bold3')}</strong>
+			{$_('contact.arrival.fribourg.text5')}
+			<a href="https://{$_('contact.arrival.fribourg.link3')}">{$_('contact.arrival.fribourg.linkText3')}</a>
+			{$_('contact.arrival.fribourg.text6')}
+</li>
+			<li>
+			<strong>{$_('contact.arrival.fribourg.bold4')}</strong>
+			{$_('contact.arrival.fribourg.text7')}
+			<a href="https://{$_('contact.arrival.fribourg.link4')}">{$_('contact.arrival.fribourg.linkText4')}</a>
+			{$_('contact.arrival.fribourg.text8')}
+			<a href="https://{$_('contact.arrival.fribourg.link5')}">{$_('contact.arrival.fribourg.linkText5')}</a>
+			{$_('contact.arrival.fribourg.dot')}
+</li>
+	</ul>
+		</div>
+			<div>
+				<h3>{$_('contact.form.title')}</h3>
+				<p>{$_('contact.form.subtitle')}</p>
+			</div>
+	</section>
+</div>
