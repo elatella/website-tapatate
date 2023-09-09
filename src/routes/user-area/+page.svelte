2d73a945a2ed694f9ab01fbfc3821d024b9d0fd5
Lena Fuhrimann <6780471+cloudlena@users.noreply.github.com>
2023-09-09 12:49:23 +0200

    Add user area content

--- src/routes/user-area/+page.svelte
+++ src/routes/user-area/+page.svelte
@@ -9,8 +9,11 @@
 <div class="w-full px-8">
 	<section class="container mx-auto flex flex-col max-w-5xl space-y-16">
 		<div class="space-y-4">
-			<h3 class="h3">{$_('farm.farm.title')}</h3>
-			<p>{$_('farm.farm.body1')}</p>
+			<h3 class="h3">{$_('userArea.adminTool.title')}</h3>
+			<a href="https://admin.tapatate.ch" class="btn variant-filled"
+				>{$_('userArea.adminTool.title')}</a
+			>
+			<p>{$_('userArea.adminTool.description')}</p>
 		</div>
 	</section>
 </div>
