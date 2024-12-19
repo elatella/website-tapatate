8472f10ad6496a8c0b6ebffe3011ef015362d8d1
TaPatate <157107201+TaPatate@users.noreply.github.com>
2024-12-19 17:26:22 +0100

    Change entry order depots.ts

--- src/lib/depots.ts
+++ src/lib/depots.ts
@@ -7,10 +7,10 @@ export interface Depot {
 
 const depots: Depot[] = [
 	{
-		name: 'Düdingen Robinsonspielplatz',
-		location: 'Düdingen',
-		address: 'Leimacker 1, 3186 Düdingen',
-		coordinates: [46.846006, 7.199499]
+		name: 'Alpenegg',
+		location: 'Bern',
+		address: 'Alpeneggstrasse 18, 3012 Bern',
+		coordinates: [46.95377, 7.4381]
 	},
 	{
 		name: 'Bern West',
@@ -19,22 +19,10 @@ const depots: Depot[] = [
 		coordinates: [46.94845, 7.39664]
 	},
 	{
-		name: 'Tiefenau',
+		name: 'Donnerbühlweg',
 		location: 'Bern',
-		address: 'Kastellweg 7, 3004 Bern',
-		coordinates: [46.97395, 7.45077]
-	},
-	{
-		name: 'Gerbegasse',
-		location: 'Kerzers',
-		address: 'Gerbegasse 18, 3210 Kerzers',
-		coordinates: [46.97473, 7.19676]
-	},
-	{
-		name: 'Bluefactory',
-		location: 'Fribourg',
-		address: 'Bluefactory, NeighborHub, Passage du Cardinal 1, 1700 Fribourg',
-		coordinates: [46.79745, 7.14715]
+		address: 'Donnerbühlweg 31, 3012 Bern',
+		coordinates: [46.95119, 7.43178]
 	},
 	{
 		name: 'Fischermätteli',
@@ -43,22 +31,10 @@ const depots: Depot[] = [
 		coordinates: [46.94093, 7.4198]
 	},
 	{
-		name: 'Donnerbühlweg',
-		location: 'Bern',
-		address: 'Donnerbühlweg 31, 3012 Bern',
-		coordinates: [46.95119, 7.43178]
-	},
-	{
-		name: 'Alpenegg',
-		location: 'Bern',
-		address: 'Alpeneggstrasse 18, 3012 Bern',
-		coordinates: [46.95377, 7.4381]
-	},
-	{
-		name: 'Q-Laden',
-		location: 'Bern',
-		address: 'Bioladen Q-Laden, Quartierhof 1, 3013 Bern',
-		coordinates: [46.95838, 7.44662]
+		name: 'Gerbegasse',
+		location: 'Kerzers',
+		address: 'Gerbegasse 18, 3210 Kerzers',
+		coordinates: [46.97473, 7.19676]
 	},
 	{
 		name: 'Löscher',
@@ -73,10 +49,34 @@ const depots: Depot[] = [
 		coordinates: [46.94117, 7.4705]
 	},
 	{
+		name: 'Q-Laden',
+		location: 'Bern',
+		address: 'Bioladen Q-Laden, Quartierhof 1, 3013 Bern',
+		coordinates: [46.95838, 7.44662]
+	},
+	{
+		name: 'Tiefenau',
+		location: 'Bern',
+		address: 'Kastellweg 7, 3004 Bern',
+		coordinates: [46.97395, 7.45077]
+	},
+	{
 		name: 'Via Felsenau',
 		location: 'Bern',
 		address: 'Via Felsenau, Spinnereiweg 4, 3004 Bern',
 		coordinates: [46.97006, 7.44215]
+	},
+	{
+		name: 'Düdingen Robinsonspielplatz',
+		location: 'Düdingen',
+		address: 'Leimacker 1, 3186 Düdingen',
+		coordinates: [46.846006, 7.199499]
+	},
+	{
+		name: 'Bluefactory',
+		location: 'Fribourg',
+		address: 'Bluefactory, NeighborHub, Passage du Cardinal 1, 1700 Fribourg',
+		coordinates: [46.79745, 7.14715]
 	},
 	{
 		name: 'Wallenbuch',
